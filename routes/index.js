3721fd1740ae281e9d77ce56df35e50b7027b30b
townmi <towne766@126.com>
2015-01-30 18:55:44 +0800

    18:55

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var Read = require("./readfile.js");
 
-<<<<<<< HEAD
 var router = express.Router();
 
 router.get("/", function (req, res){
@@ -11,9 +10,11 @@ router.get("/", function (req, res){
 
 	read.seach(function(){
 
-		// res.send(read.data);
+		res.send(read.data);
 
-		res.render("index", { "title": "index"});
+		// res.render("index", { "title": "index"});
+
+		//res.sendFile("/index.ejs");
 
 		
 
@@ -39,125 +40,4 @@ router.get("/css", function (req, res){
 
 
 module.exports = router;
-=======
-// var basePath = "md";
-// var fs = require('fs');
-var router = express.Router();
-// var myart = [], art_js = [], art_html = [], art_css = [], art_node = [], art_db = [], art_c = [], art_life = [];
 
-// fs.readdir(basePath, function (err, all){
-// 	if (err) throw err;
-// 	var i_num = 0;
-// 	all.forEach(function(i){
-// 		fs.readFile(basePath+'/'+i, function (err, data){
-// 			if(err) throw err;
-// 			i_num++;
-// 			var strTwo = data.toString().split("======");
-// 			myart.push( [eval(strTwo[0])[0], markdown.toHTML(strTwo[1]) ] );
-// 			if(i_num === all.length){
-// 				myart.sort(function(a, b){
-// 					// 按日期排序
-// 					var firstD = new Date(), nextD = new Date();
-// 					var first = a[0]["date"].split('-');
-// 					var next = b[0]["date"].split('-');
-// 					firstD.setFullYear(first[0],first[1],first[2]);
-// 					nextD.setFullYear(next[0],next[1],next[2]);
-// 					return nextD - firstD;
-// 				});
-				
-// 				myart.forEach(function(i){
-// 					// 抽取javascript分类
-// 					if(i[0]['kate'] === "javascript"){
-// 						art_js.push(i);
-// 					}
-// 					// 抽取html分类
-// 					if(i[0]['kate'] === "html"){
-// 						art_html.push(i);
-// 					}
-// 					// 抽取css分类
-// 					if(i[0]['kate'] === "css"){
-// 						art_css.push(i);
-// 					}
-// 					// 抽取node分类
-// 					if(i[0]['kate'] === "nodejs"){
-// 						art_node.push(i);
-// 					}
-// 					// 抽取db分类
-// 					if(i[0]['kate'] === "db"){
-// 						art_db.push(i);
-// 					}
-// 					// 抽取c分类
-// 					if(i[0]['kate'] === "c"){
-// 						art_c.push(i);
-// 					}
-// 					// 抽取life分类
-// 					if(i[0]['kate'] === "life"){
-// 						art_life.push(i);
-// 					}
-// 				});
-// 				// route contorl
-// 				// get /
-// 				router.get('/' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog", "myart" : myart });
-					
-// 				})
-// 				// get /js
-// 				router.get('/js' ,function (req,  res, next){
-					
-// 					res.render('index', {"title" : "Towne's Blog - javascript", "myart" : art_js });
-					
-// 				})
-// 				// get /html
-// 				router.get('/html' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - html", "myart" : art_html });
-					
-// 				})
-// 				// get /css
-// 				router.get('/css' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - css", "myart" : art_css });
-					
-// 				})
-// 				// get /node
-// 				router.get('/node' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - node.js", "myart" : art_node });
-					
-// 				})
-// 				// get /db
-// 				router.get('/db' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - MongoDB", "myart" : art_db });
-					
-// 				})
-// 				// get /c
-// 				router.get('/c' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - c++", "myart" : art_c });
-					
-// 				})
-// 				// get /life
-// 				router.get('/life' ,function (req,  res, next){
-					
-// 					res.render('index', { "title" : "Towne's Blog - life", "myart" : art_life });
-					
-// 				})
-// 			}
-// 		});
-// 	});
-
-// });
-
-var read = new Read();
-
-read.seach(function(){
-	console.log(this);
-})
-
-module.exports = router;
-
-			
-			
->>>>>>> 343610165ebdec3b731397711a06cbfc0d4faa2c
--- routes/readfile.js
+++ routes/readfile.js
@@ -12,34 +12,17 @@ Read.prototype.seach = function(callback) {
 
 	var _this = this;
 
-<<<<<<< HEAD
-	this.name = "javascript";
 
 	if(this.name && this.name.split("-").length>1){
 		
 		fs.readFile(_this.basePath +"/"+ _this.name+".md", function (err, data){
-=======
-	fs.readdirSync(this.basePath, function (err, files){
 
-		files.forEach(function (i, k){
->>>>>>> 343610165ebdec3b731397711a06cbfc0d4faa2c
 
 			_this.data.push(data);
 
-<<<<<<< HEAD
+
 			callback();
 
-=======
-				_this.num++;
-
-				_this.data.push(data);
-
-				if(_this.num >= files.length){
-					callback();
-				}
-
-			})
->>>>>>> 343610165ebdec3b731397711a06cbfc0d4faa2c
 		})
 
 	}else{
