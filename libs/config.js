f9dddf9967685da0546c3d0522f36688046e1fe4
townmi <towne766@126.com>
2018-03-15 14:25:45 +0800

    rm

--- libs/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	basePath: "http://10.106.89.64:3000/"
-};
-
--- models/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"host" : "127.0.0.1",
-	"port" : 3306,
-	"user": "root",
-	// vps
-	"password" : "",
-	"db": "blog"
-}
